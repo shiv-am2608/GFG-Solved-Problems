5d62fb1dca8038f6aec01a9d0a66a6d371e9b587
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-11-09 14:26:32 +0530

    Added solution

---
+++ Largest number in K swaps - GFG/largest-number-in-k-swaps.java
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int k = sc.nextInt();
+                    String str = sc.next();
+                    Solution obj = new Solution();
+                    System.out.println(obj.findMaximumNum(str, k));
+                }
+        }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to find the largest number after k swaps.
+    static String max;
+    public static void permute(String str,int k)
+    {
+        if(max.compareTo(str)<0)
+            max=str;
+        if(k==0)
+            return;
+        char ch[]=str.toCharArray();
+        for(int i=0;i<str.length();i++)
+        {
+            int index=i;
+            for(int j=i+1;j<str.length();j++)
+            {
+                if(ch[i]<ch[j])
+                {
+                    char temp=ch[j];
+                    ch[j]=ch[index];
+                    ch[index]=temp;
+                    permute(String.valueOf(ch),k-1);
+                    temp=ch[j];
+                    ch[j]=ch[index];
+                    ch[index]=temp;
+                }
+            }
+        }
+        return ;
+    }
+    public static String findMaximumNum(String str, int k)
+        {
+            //code here.
+            max="-1";
+            permute(str,k);
+            return max;
+        }
+}
