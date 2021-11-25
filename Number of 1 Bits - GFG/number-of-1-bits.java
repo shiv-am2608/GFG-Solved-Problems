6585f4946195742b88e09a94199cacc0ab0c0d92
Shivam Agrawal <72264862+shiv-am2608@users.noreply.github.com>
2021-11-25 12:21:46 +0530

    Added solution

--- Number of 1 Bits - GFG/number-of-1-bits.java
+++ Number of 1 Bits - GFG/number-of-1-bits.java
@@ -24,10 +24,10 @@ class Solution {
     static int setBits(int N) {
         // code here
         int count=0;
-        while(N!=0)
+        while(N>0)
         {
-            count+=N&1;
-            N=N>>1;
+            count++;
+            N=N&(N-1);
         }
         return count;
     }
